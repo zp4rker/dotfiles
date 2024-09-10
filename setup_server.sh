1eecdabc27cb0336d1afbc909f6b1467ff3c5f2d
Zaeem Parker <iam@zp4rker.com>
2024-09-10 19:11:39 +1000

    change auth key crontab to run 6-hourly

--- setup_server.sh
+++ setup_server.sh
@@ -16,7 +16,7 @@ mkdir -p ~/.ssh
 chmod u=rwx,og= ~/.ssh
 curl https://github.com/zp4rker.keys -o ~/.ssh/authorized_keys >/dev/null 2>&1
 chmod u=rw,og= ~/.ssh/authorized_keys
-(crontab -l 2>/dev/null; echo "0 0 * * 0 curl https://github.com/zp4rker.keys -o ~/.ssh/authorized_keys >/dev/null 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "0 */6 * * * curl https://github.com/zp4rker.keys -o ~/.ssh/authorized_keys >/dev/null 2>&1") | crontab -
 EOS
 echo "zp4rker ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/zp4rker
 sudo chmod ug=r,o= /etc/sudoers.d/zp4rker
